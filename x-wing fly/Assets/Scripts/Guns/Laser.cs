959bcae1afd861a02c7b1d9e7f67e3bedd78f95b
Kasparas <kasparas.kazemekas@gmail.com>
2020-08-26 16:46:27 +0300

    Fly scrip split and cleaned up

---
+++ x-wing fly/Assets/Scripts/Guns/Laser.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Normal.Realtime;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public Realtime _realtime;
+    public GameObject laserPref;
+    public Transform laserOrigin;
+
+    private float _projectileSpeed;
+    private float _timeBetweenShots = 0.1f;
+    private float _projectileDuration = 1;
+
+    float nextTimeFire = 0;
+
+    private bool _initializedFromServer;
+
+    void Start()
+    {
+        LoadSettings();
+    }
+
+    void LoadSettings()
+    {
+        GameLoading loader = RemoteUnityManager.Instance.GetComponent<GameLoading>();
+        RemoteUnityManager unityRemote = RemoteUnityManager.Instance;
+        
+        if (!loader.loadingDone)
+            unityRemote.onLaserDataUpdated += Initialize;
+        else
+        {
+            Initialize(unityRemote.projectileSpeed, unityRemote.projectileDuration, unityRemote.timeBetweenShots, true);
+        }
+
+    }
+
+    public void Initialize(float projectileSpeed, float projectileDuration, float timeBetweenShots, bool initializedFromServer)
+    {
+        if (!_initializedFromServer)
+        {
+            _projectileSpeed = projectileSpeed;
+            _projectileDuration = projectileDuration;
+            _timeBetweenShots = timeBetweenShots;
+        }
+
+        _initializedFromServer = initializedFromServer;
+    }
+
+    public void FireLaser(ref int energy)
+    {
+        GameObject projectile = Realtime.Instantiate(laserPref.name, laserOrigin.position, laserOrigin.rotation,
+            ownedByClient: true, useInstance: _realtime);
+
+        Projectile proj = projectile.GetComponent<Projectile>();
+        proj.Initialize(_projectileDuration);
+        proj.Fire(laserOrigin.forward * _projectileSpeed);
+
+        energy--;
+
+  //      PlaySound(0);
+
+        nextTimeFire = Time.time + _timeBetweenShots;
+        
+    }
+
+    public bool AllowToFire(int energy)
+    {
+        return ShotReloaded() && energy > 0;
+    }
+
+    public bool ShotReloaded()
+    {
+        return Time.time >= nextTimeFire;
+    }
+}
